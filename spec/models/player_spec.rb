58fee6355227ec83e3995b354afeae50e5485bf8
Gregor <gregor.afc@gmail.com>
2015-03-17 21:11:46 +0100

    Rspec configuration. Test for player validations

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Player, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Player do
+  context 'validations' do
+    it { should validate_presence_of :firstname }
+    it { should validate_presence_of :lastname }
+  end
+  
 end
