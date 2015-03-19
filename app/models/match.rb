551220cbd6a7d76645e835f192ed07b618ff1fd1
Gregor <gregor.afc@gmail.com>
2015-03-19 21:41:43 +0100

    Add validations for Match model and seeds.

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,11 @@
 class Match < ActiveRecord::Base
   belongs_to :home_player
   belongs_to :away_player
+
+  validates :home_player_id,
+            :away_player_id,
+            :looser_score,
+            :match_date, presence: true
+  validates :looser_score, inclusion: { in: 0..9 }
+  validates :home_player_win, inclusion: [ true, false ]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,28 @@
 Player.delete_all
 Operator.delete_all
+Match.delete_all
 
 Operator.create!( login: Rails.application.secrets.login,
                   password: Rails.application.secrets.password )
-
 
 10.times do
   Player.create!(firstname: Faker::Name.first_name,
                 lastname: Faker::Name.last_name,
                 avatar: Faker::Avatar.image )
+  puts 'Player'
+end
+
+Player.all.each do |home_player|
+  Player.all.each do |away_player|
+    unless home_player == away_player
+      home_player_win = rand(2) == 1
+      looser_score = rand(10)
+      Match.create!( match_date: Faker::Date.backward(50),
+                     home_player_win: home_player_win,
+                     looser_score: looser_score,
+                     home_player_id: home_player.id,
+                     away_player_id: away_player.id )
+      puts 'Match'
+    end
+  end
 end
