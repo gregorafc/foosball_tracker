376eb0613d3ed7096a8f467e990112fedc1485a9
Gregor <gregor.afc@gmail.com>
2015-03-19 22:05:18 +0100

    Add specs for Match model. Test validations and relations.

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,11 +1,10 @@
 class Match < ActiveRecord::Base
-  belongs_to :home_player
-  belongs_to :away_player
+  belongs_to :home_player, class_name: 'Player'
+  belongs_to :away_player, class_name: 'Player'
 
   validates :home_player_id,
             :away_player_id,
             :looser_score,
             :match_date, presence: true
   validates :looser_score, inclusion: { in: 0..9 }
-  validates :home_player_win, inclusion: [ true, false ]
 end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Match, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Match do
+  context 'validations' do
+    it { should validate_presence_of :match_date }
+    it { should validate_presence_of :looser_score }
+    it { should validate_presence_of :home_player_id }
+    it { should validate_presence_of :away_player_id }
+    it { should validate_inclusion_of(:looser_score).in_range(0..9) }
+  end
+
+  context 'relations' do
+    it { should belong_to(:home_player).class_name('Player') }
+    it { should belong_to(:away_player).class_name('Player') }
+  end
+
 end
