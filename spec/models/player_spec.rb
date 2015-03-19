a72b897b9d222c74522381648b6e6314f416244c
Gregor <gregor.afc@gmail.com>
2015-03-19 22:08:39 +0100

    Spec for relations in Player model.

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -9,5 +9,10 @@ describe Player do
     it { should validate_attachment_size(:avatar).
                 less_than(300.kilobytes) }
   end
+
+  context 'relations' do
+    it { should have_many(:home_matches).class_name('Match') }
+    it { should have_many(:away_matches).class_name('Match') }
+  end
   
 end
