5e3a35d2b38da7c301c8df0e968cf8226ca906e6
Gregor <gregor.afc@gmail.com>
2015-03-20 11:33:50 +0100

    Add assatiatins: home_win, home_loses, away_win, home_loses to model Player. Add methods: matches, wins, loses, goals_by_win, goals_by_lose, goals, points, avg_opp_rating.

--- app/models/player.rb
+++ app/models/player.rb
@@ -10,4 +10,48 @@ class Player < ActiveRecord::Base
                        :size => { :in => 0..300.kilobytes }
 
 
+  has_many :home_wins, -> { where('home_player_win = ?', true) }, foreign_key: :home_player_id, class_name: 'Match'
+  has_many :home_loses, -> { where('home_player_win = ?', false) }, foreign_key: :home_player_id, class_name: 'Match'
+  has_many :away_wins, -> { where('home_player_win = ?', false) }, foreign_key: :away_player_id, class_name: 'Match'
+  has_many :away_loses, -> { where('home_player_win =?', true) }, foreign_key: :away_player_id, class_name: 'Match'
+
+  def matches
+    home_matches + away_matches
+  end
+
+  def wins
+    home_wins + away_wins
+  end
+
+  def loses
+    home_loses + away_loses
+  end
+
+  def goals_by_win
+    wins.count * 10
+  end
+
+  def goals_by_lose
+    loses.map(&:looser_score).inject(:+)
+  end
+
+  def goals
+    goals_by_win + goals_by_lose
+  end
+
+  def points
+    wins.count * 3
+  end
+
+  def rating
+    unless matches == []
+      (points*0.7 + goals*0.2 - matches.count*0.1 ).round(2)
+    end
+  end
+
+  def avg_opp_rating
+    tab = home_matches.map(&:away_player_id) + away_matches.map(&:home_player_id)
+    tab.map { |player| Player.find(player).rating }.inject(:+) / tab.size
+  end
+
 end
