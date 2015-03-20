c39554f5ad8f01db4c03492943ebf2da57c5debd
Gregor <gregor.afc@gmail.com>
2015-03-20 17:26:26 +0100

    Add callback after_save rate_update to Match.

--- app/models/match.rb
+++ app/models/match.rb
@@ -8,5 +8,10 @@ class Match < ActiveRecord::Base
             :match_date, presence: true
   validates :looser_score, inclusion: { in: 0..9 }
 
+  after_save :rate_update
 
+  def rate_update
+    self.home_player.rate
+    self.away_player.rate
+  end
 end
